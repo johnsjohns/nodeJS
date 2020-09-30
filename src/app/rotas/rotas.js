c956ec82e716fc09d7260f30606ed70a472de9f7
johnsjohns <o-ana@hotmail.com>
2020-09-30 19:52:23 -0300

    Começo aula 3

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,34 +1,28 @@
+const db = require('../../config/database');
 module.exports = (app) => {
 
 
     app.get('/', function(req, resp) {
         resp.send(`
-    <html>
-        <head>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1> Casa do Código </h1>
-        <body>
-    <html>
-`);
+                 <html>
+                        <head>
+                            <meta charset="utf-8">
+                        </head>
+                        <body>
+                            <h1> Casa do Código </h1>
+                        <body>
+                    <html>
+                `);
     });
 
     app.get('/livros', function(req, resp) {
-        resp.marko(
-            require('../views/livros/lista/lista.marko'),
-            {
-                livros:[
-                    {
-                        id: 1,
-                        titulo: 'Fundamentos do Node'
-                    },
-                    {
-                        id: 2,
-                        titulo:'Node avançado'
-                    }
-                ]
-            }
-        );
+        db.all('SELECT * FROM livros', function(erro, resultados) {
+            resp.marko(
+                require('../views/livros/lista/lista.marko'),
+                {
+                    livros: resultados
+                }
+            );
+        });
     });
 };
