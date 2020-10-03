9b93b72a350e0adf1be346b26dad8d9335db4e83
johnsjohns <o-ana@hotmail.com>
2020-10-02 23:08:34 -0300

    Aula 5 - adicionando remover

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -38,5 +38,15 @@ module.exports = (app) => {
         livroDao.adiciona(req.body)
             .then(resp.redirect('/livros'))
             .catch(erro => console.log(erro));
-    })
+    });
+
+    app.delete('/livros/:id', function (req, resp){
+        const id = req.params.id;
+        const livroDao = new LivroDao(db);
+        livroDao.remove(id).then(()=> resp.status(200).end()).catch(erro => console.log(erro))
+    });
+
+   
+
+
 };
--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -5,9 +5,18 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+ });
+
+app.use('/estatico', express.static('./src/app/public/'));
+
 app.use(bodyParser.urlencoded({
     extended: true
-}))
+}));
 
 const rotas = require('../app/rotas/rotas');
 rotas(app);
