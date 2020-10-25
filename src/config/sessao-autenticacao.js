c1c7c509621d50f789dd1f9c502fc4729b73b91c
johnsjohns <o-ana@hotmail.com>
2020-10-25 18:19:00 -0300

    Finalizado arquivo de configuracao de login

--- src/config/sessao-autenticacao.js
+++ src/config/sessao-autenticacao.js
@@ -13,7 +13,7 @@ module.exports = (app)=> {
         },
         (email, senha, done) => {
             const usuarioDao = new usuarioDao(db);
-            usuarioDao.buscPorEmail(email).then(usuario => {
+            usuarioDao.buscaPorEmail(email).then(usuario => {
                 if(!usuario || senha != usuario.senha) {
                     return done(null, false, {
                         mensagem: 'Login e senha incorretos"'
@@ -24,4 +24,29 @@ module.exports = (app)=> {
             }).catch(erro => done(erro, false));
         }
     ));
-}
+
+    passport.serializeUser((usuario, done) => {
+        const usuarioSessao = {
+        nome: usuario.nome_completo,
+        email: usuario.email
+        }
+
+        done(null, usuarioSessao);
+    });
+
+    passport.deserializeUser((usuarioSessao, done) => {
+        done(null, usuarioSessao);
+    });
+
+    app.use(sessao({
+        secret: 'node alura',
+        genid: function(req){
+            return uuid();
+        },
+        resave: false,
+        saveUninitialized: false,
+    }));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+};
